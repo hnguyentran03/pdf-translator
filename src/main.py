34c8883fea28080f231ebb9ecb75112dfc5cb17d
Ben <hnguyent@andrew.cmu.edu>
2024-07-31 13:19:22 +0700

    Implemented writing translated text onto the pdf.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import openai
 import pathlib
 import pymupdf
 
-def get_system_prompt(path):
+def get_system_prompt(path: str):
     """Retrieves the system prompt for the system message from path."""
     system_prompt_file = pathlib.Path(path)
     with open(system_prompt_file, "r") as f:
@@ -26,7 +26,7 @@ def whiteout_block(page: pymupdf.TextPage, block: dict):
             page.draw_rect(span["bbox"], fill=(1, 1, 1), width=0)
 
 
-def get_span_from_block(block):
+def get_span_from_block(block: dict):
     """
     Gets the text information from the span in a given block.
 
@@ -82,6 +82,71 @@ def query(client: openai.OpenAI,
     message = completion.choices[0].message.content
     return message
 
+def insert_text_dynamic(page: pymupdf.TextPage, 
+                        text: str, 
+                        bbox: tuple, 
+                        max_size: int
+):
+    """
+    Writes text onto the page, adjusting the length to ensure it fits inside a bounding box.
+
+    Arguments
+    ---------
+    page: pymupdf.TextPage
+        The page to be written on.
+    text: str
+        The text that will be written on the page.
+    bbox: tuple
+        A tuple (x0, y0, x1, y1) of the bounding box of the text.
+    max_size: int
+        The maximum font size for the text.
+    """
+    x0, y0, x1, y1 = bbox
+    width = x1 - x0
+    height = y1 - y0
+
+    font_size = 1
+    text_width, text_height = pymupdf.get_text_length(text, fontsize=font_size), font_size
+
+    # Incrementally increase font size until the text no longer fits
+    while text_width < width and text_height < height and font_size <= max_size:
+        font_size += 1
+        text_width, text_height = pymupdf.get_text_length(text, fontsize=font_size), font_size
+
+    font_size -= 1
+
+    # Text alignment
+    text_width, text_height = pymupdf.get_text_length(text, fontsize=font_size), font_size
+    x = x0 + 1  # + 1 for padding
+    y = y0 + (height - text_height) / 2 + text_height
+
+    page.insert_text(pymupdf.Point(x, y),
+                     text,
+                     fontsize=font_size,
+                     )
+
+def replace_text(page: pymupdf.TextPage, 
+                 spans: dict, 
+                 replacement_texts: dict
+):
+    """
+    Writes each of the translate text on top of the whiteout area based on the span.
+
+    Arguments
+    ---------
+    page: pymupdf.TextPage
+        The page to be written on.
+    spans: dict
+        A dictionary mapping {pos: text_info} where text_info is a dict containing the position, font, etc. of the text.
+    replacement_texts: dict
+        A dictionary mapping {pos: translated_text} where pos corresponds to the pos in spans.
+    """
+    for pos, replacement_text in replacement_texts.items():
+        pos = int(pos) # Because spans initialized pos as an int
+        if pos in spans:
+            span = spans[pos]
+            insert_text_dynamic(page, replacement_text,
+                                span["bbox"], span["size"])
 
 def main():
     client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -91,8 +156,10 @@ def main():
     system_prompt = get_system_prompt("src/prompts/system_prompt.txt")
     system_prompt = system_prompt.replace("{language}", language)
 
+    # Because the pdfs are in the folder `pdfs/{filename}` for organization
     filename = input("Which PDF file do you want to translate (name of the folder/file)? ")
     input_path = pathlib.Path(f"pdfs/{filename}/{filename}.pdf")
+
     with pymupdf.open(input_path) as doc:
         for page in doc:
             page_info = page.get_text("dict")
@@ -102,17 +169,16 @@ def main():
                     whiteout_block(page, block)
                     spans.extend(get_span_from_block(block))
             
-            spans = dict(enumerate(spans))
+            spans = dict(enumerate(spans)) # Tries to maintain the position of the text after translation by enumerating them
             texts = {pos: span["text"] for pos, span in spans.items()}
 
             response = query(client, json.dumps(texts),system_prompt=system_prompt)
             translated_texts = json.loads(response)
-
-            print(translated_texts)
+            replace_text(page, spans, translated_texts)
         
-        # output_path = pathlib.Path(f"pdfs/{filename}/{filename}_result.pdf")
-        # doc.save(output_path)
-        # print("Done!")
+        output_path = pathlib.Path(f"pdfs/{filename}/{filename}_result.pdf")
+        doc.save(output_path)
+        print("Done!")
     
 
 if __name__ == "__main__":
